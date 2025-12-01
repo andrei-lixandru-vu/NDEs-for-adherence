148df916680d81edd52b27561fdbff5f4e0b82fb
andrei-lixandru-vu <a.v.lixandru@vu.nl>
2025-12-01 14:21:12 +0100

    Update data_generation.py

--- clean_ndes/data_generation.py
+++ clean_ndes/data_generation.py
@@ -90,3 +90,46 @@ def generate_synthetic_data_super_simple(N, T, D):
             else:
                 data[n, t, 0] = 1
     return data, adherence_dropped_at
+
+
+def generate_synthetic_data_super_simple_Interactions2StepsApart(N, T, D):
+    """
+    Generate simplified synthetic data.
+    
+    Adherence drops to 0 if reminder factor 1 (index 1) is 1 two timepoints before.
+    
+    Parameters
+    ----------
+    N : int
+        Number of trajectories (patients)
+    T : int
+        Number of time steps (days)
+    D : int
+        Number of dimensions
+    
+    Returns
+    -------
+    data : np.ndarray
+        Shape (N, T, D) containing trajectory data
+    adherence_dropped_at : list
+        List of (patient_idx, timestep) tuples where adherence dropped
+    """
+    data = np.zeros((N, T, D))
+    adherence_dropped_at = []
+
+    # Set dims 1-6 to random 0 or 1 (50% each)
+    data[:, :, 1:] = np.random.randint(0, 2, size=(N, T, D-1))
+    
+    # For each trajectory (patient)
+    for n in range(N):
+        # Set initial adherence at t=0 to 1
+        data[n, 0, 0] = 1
+
+        for t in range(1, T):
+            # At t, check dim 1 at t-2
+            if data[n, t-2, 1] == 1:
+                data[n, t, 0] = 0
+                adherence_dropped_at.append((n, t))
+            else:
+                data[n, t, 0] = 1
+    return data, adherence_dropped_at
